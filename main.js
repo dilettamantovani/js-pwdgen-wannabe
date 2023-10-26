3da57d1514ca83629827efc519e6cacfe284ecc0
dilettits <mantovani.d3@gmail.com>
2023-10-26 10:25:40 +0200

    added comments during exercise correction

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 
-const userName = prompt("Scrivi il tuo nome");
-const userSurname = prompt("Scrivi il tuo cognome");
-const userFavColor = prompt("Scrivi il tuo colore preferito");
+const firstName = prompt("Scrivi il tuo nome");
+const lastName = prompt("Scrivi il tuo cognome");
+const favColor = prompt("Scrivi il tuo colore preferito");
+const suffix = "#23";
+
+//qui Luca ha inserito dei console.log e poi ha poi decommentato i primi 3:
+//console.log("firstName", firstName)
+//console.log("lastName", lastName)
+//console.log("favColor", favColor)
+//console.log("suffix", suffix)
 
 let genPassword = userName + userSurname + userFavColor + "#23"
+
+//Luca ha fatto const password = userName + userSurname + userFavColor + "#23"
 
 document.getElementById("welcome").innerHTML = "Benvenuto, " + userName;
 document.getElementById("password").innerHTML = "La tua password generata è " + genPassword;
